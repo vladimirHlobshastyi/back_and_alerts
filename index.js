e8b307790d434baef9d5faad3a24c30776a651f0
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2022-07-11 17:17:18 +0300

    fixed elements

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ function parseMessages(messages) {
     if (isNewMessagesResponse(result)) {
       const parsedMessages = parseMessages(result.newMessages);
       //add new message document for debugging
-      await writeMessagesToFile(parsedMessages, newMessage);
+     // await writeMessagesToFile(parsedMessages, newMessage);
       try {
         for (let oneRegion of parsedMessages) {
           await client
